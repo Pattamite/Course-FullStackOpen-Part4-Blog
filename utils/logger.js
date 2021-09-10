de4e0100c13b78cccc7dc3903b7edac79631cd87
pattamite <jittapat007@gmail.com>
2021-09-10 11:19:21 +0700

    Create simple backend of blog apps

---
+++ utils/logger.js
@@ -0,0 +1,14 @@
+function info(...params) {
+  console.log(...params);
+}
+
+function error(...params) {
+  console.error(...params);
+}
+
+const logger = {
+  info,
+  error,
+};
+
+module.exports = logger;
